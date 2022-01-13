38e10d3d889ac351221e82dbde795f5266722665
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-01-13 20:04:19 +0300

    update

--- digital/led-chaser/led-chaser.c
+++ digital/led-chaser/led-chaser.c
@@ -3,36 +3,56 @@
  *
  * Created: 8/10/2014 22:42:36
  *  Author: Muchiri john
- */ 
+ */
 
-#include <sketch.h>
-#include <pin.h>
+
+#define F_CPU 8000000L
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+#include "pin.h"
 
 uint8_t byt = 1;
 uint8_t counter = 0;
 
+
+/************************************************************************
+* initialization code
+************************************************************************/
 void init_code()
 {
-	portSet(D, 0xFF);
+	portSet(D, 0xFF); //set PORTD to output
 }
 
-void main_code()
+/************************************************************************
+* main function
+************************************************************************/
+int main()
 {
-	for(byt = 0; byt<=7; byt++)
-	{
-		portWrite(D, (1 << byt));
-		_delay_ms(250);
+	//init
+	init_code();
+	//loop forever
+	while(1){
+		//led chaser
+		for(byt = 0; byt<=7; byt++)
+		{
+			portWrite(D, (1 << byt));
+			_delay_ms(250);
+		}
+
+		for(byt = 0; byt<=7; byt++)
+		{
+			portWrite(D, (0x80 >> byt));
+			_delay_ms(250);
+		}
+
+		for(byt=0; byt<=255; byt++)
+		{
+			portWrite(D, byt);
+			_delay_ms(250);
+		}
 	}
-	
-	for(byt = 0; byt<=7; byt++)
-	{
-		portWrite(D, (0x80 >> byt));
-		_delay_ms(250);
-	}
-	
-	for(byt=0; byt<=255; byt++)
-	{
-		portWrite(D, byt);
-		_delay_ms(250);
-	}
+	return 0;
 }
