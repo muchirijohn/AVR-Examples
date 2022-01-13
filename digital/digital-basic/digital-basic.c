73f4a1b89957da80d86b32e28db8cbcbd8f827bd
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-01-13 20:00:30 +0300

    update

--- digital/digital-basic/digital-basic.c
+++ digital/digital-basic/digital-basic.c
@@ -3,26 +3,44 @@
  *
  * Created: 8/10/2014 22:39:23
  *  Author: Muchiri john
- */ 
+ */
 
+#define F_CPU 8000000L
 
-#include <sketch.h>
-#include <pin.h>
+#include <avr/io.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+#include "pin.h"
 
 uint8_t on= 0xFF;
 uint8_t off= 0x00;
 
 uint8_t counter = 0;
 
+
+/************************************************************************
+* initialization code
+************************************************************************/
 void init_code()
-{	
-	portSet(D, 0xFF);
+{
+	portSet(D, 0xFF); //set PORTD [D0--D7] as output
 }
 
-void main_code()
+
+/************************************************************************
+* main function
+************************************************************************/
+int main()
 {
-	portWrite(D, on);
-	_delay_ms(500);
-	portWrite(D, off);
-	_delay_ms(500);
+	//init code
+	init_code();
+	//loop forever
+	while(1){
+		portWrite(D, on); //PORTD ON
+		_delay_ms(500); //delay 500ms
+		portWrite(D, off); //PORTD OFF
+		_delay_ms(500); //delay 500ms
+	}
+	return 0;
 }
