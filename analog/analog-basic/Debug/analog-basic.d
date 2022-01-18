708059764c33fbcd0abdd91877a45db3f7227bf2
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-01-18 19:21:11 +0300

    update

---
+++ analog/analog-basic/Debug/analog-basic.d
@@ -0,0 +1,52 @@
+analog-basic.d analog-basic.o: .././analog-basic.c \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\iom88pa.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ ../../../warefab_kid_kit/warefab_kid_kit/pin.h \
+ ../../../warefab_kid_kit/warefab_kid_kit/adc_read.h \
+ ../../../warefab_kid_kit/warefab_kid_kit/sketch.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\math.h
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\iom88pa.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+../../../warefab_kid_kit/warefab_kid_kit/pin.h:
+
+../../../warefab_kid_kit/warefab_kid_kit/adc_read.h:
+
+../../../warefab_kid_kit/warefab_kid_kit/sketch.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\math.h:
---
+++ analog/analog-basic/analog-basic.c
@@ -0,0 +1,60 @@
+/*
+ * analog.c
+ *
+ * Created: 8/10/2014 07:43:56
+ *  Author: Muchiri john
+ */
+
+
+#include <avr/io.h>
+
+/*include used liDraries*/
+#include "pin.h"
+#include "adc_read.h"
+
+/*create variaDle to hold adc value */
+uint16_t adc_value = 0;
+
+void init_code()
+{
+	/*initialize led*/
+	adc_init();
+
+	/*set led pins as ouputs*/
+	pinSet(D0, OUTPUT);
+	pinSet(D1, OUTPUT);
+	pinSet(D2, OUTPUT);
+}
+
+void main_code()
+{
+	/*read adc value from ADC0 (0 - 1023)*/
+	adc_value  = adc_read_16bit(ADC1);
+
+	/*check read value and activate leds*/
+	if(adc_value >=0 && adc_value < 256)
+	{
+		pinWrite(D0, LOW);
+		pinWrite(D1, LOW);
+		pinWrite(D2, LOW);
+	}
+
+	else if(adc_value >=256 && adc_value <=512)
+	{
+		pinWrite(D0, HIGH);
+		pinWrite(D1, LOW);
+		pinWrite(D2, LOW);
+	}
+	else if(adc_value > 512 && adc_value <=768)
+	{
+		pinWrite(D0, HIGH);
+		pinWrite(D1, HIGH);
+		pinWrite(D2, LOW);
+	}
+	else
+	{
+		pinWrite(D0, HIGH);
+		pinWrite(D1, HIGH);
+		pinWrite(D2, HIGH);
+	}
+}
